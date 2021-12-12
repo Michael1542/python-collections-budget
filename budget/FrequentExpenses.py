18cd8cfa8e4c63a2525c36ac209cf70948625b2a
Michael Wang <michael.yj.wang@gmail.com>
2021-12-12 13:34:33 -0500

    .

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -13,7 +13,7 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
-count, categories = zip(*top5)
+categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
 
